87ed672222c3330f7053605f61c8bb61218cb709
Phantombeaver <85520080+Phantombeaver@users.noreply.github.com>
2021-10-17 20:37:31 +0300

    Add files via upload

---
+++ Makefile
@@ -0,0 +1,67 @@
+NAME = libft.a
+
+
+
+SRC	=	ft_atoi.c \
+		ft_bzero.c \
+		ft_calloc.c \
+		ft_isalnum.c \
+		ft_isalpha.c \
+		ft_isascii.c \
+		ft_isdigit.c \
+		ft_isprint.c \
+		ft_memchr.c \
+		ft_memcmp.c \
+		ft_memcpy.c \
+		ft_memmove.c \
+		ft_memset.c \
+		ft_strchr.c \
+		ft_strdup.c \
+		ft_strjoin.c \
+		ft_strlcat.c \
+		ft_strlcpy.c \
+		ft_strlen.c \
+		ft_strncmp.c \
+		ft_strnstr.c \
+		ft_strrchr.c \
+		ft_strtrim.c \
+		ft_substr.c \
+		ft_tolower.c \
+		ft_toupper.c \
+		ft_strmapi.c \
+		ft_striteri.c \
+		ft_putchar_fd.c \
+		ft_putstr_fd.c \
+		ft_putendl_fd.c \
+		ft_putnbr_fd.c \
+		ft_itoa.c \
+		ft_split.c \
+
+HEADER	=	libft.h
+OBJ = $(SRC:%.c=%.o)
+
+CC	=	gcc
+CFLAGS = -Wall -Wextra -Werror -I$(HEADER)
+
+.PHONY: all clean fclean re bonus
+
+all: $(NAME)
+
+$(NAME): $(OBJ)
+
+$(NAME):	$(OBJ) $(HEADER)
+	ar rcs $(NAME) $?
+
+%.o : %.c $(HEADER)
+	$(CC) $(CFLAGS) -c $< -o $@
+
+
+clean:
+	@rm -f $(OBJ)
+	@echo "OBJ deleted"
+
+fclean: clean
+	@$(RM) $(NAME)
+	@echo "$(NAME) deleted"
+
+re: fclean all
---
+++ ft_atoi.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gjacinta <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/11 16:14:15 by gjacinta          #+#    #+#             */
+/*   Updated: 2021/10/11 16:14:18 by gjacinta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char	*str)
+{
+	long			res;
+	long			sign;
+	unsigned int	i;
+
+	res = 0;
+	sign = 1;
+	i = 0;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'
+		|| str[i] == '\r' || str[i] == '\v' || str[i] == '\f')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = res * 10 + (str[i] - '0');
+		i++;
+	}
+	return ((int)(sign * res));
+}
